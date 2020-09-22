845c4455db2c6be4b1753d92ac4b7fe1c187d385
Glen Padua <glen.p@synup.com>
2020-09-23 01:09:49 +0530

    Remove .next from repo

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/work":["static\u002Fchunks\u002Fpages\u002Fwork.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
