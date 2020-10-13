01caa2f506fc410b0f9839a585149406477066fb
Glen Padua <glen.p@synup.com>
2020-10-13 19:36:13 +0530

    Add links to footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,51 +1,115 @@
 import styled from 'styled-components';
+import { SiGithub, SiLinkedin, SiInstagram, SiTwitter } from 'react-icons/si';
 
 const Wrapper = styled.footer`
   width: 100%;
   display: flex;
-  height: 100px;
+  height: 150px;
   align-items: center;
   justify-content: center;
-  font-size: 0.8em;
+  flex-direction: column;
+  font-size: 0.7em;
 
   @media only screen and (min-width: 600px) {
-    font-size: 1em;
+    font-size: 0.9em;
   }
 `;
 
+const Text = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Logo = styled.img`
-  width: 30px;
+  width: 25px;
   height: auto;
   margin-right: 10px;
 `;
 
 const StyledLink = styled.a`
   text-decoration: none;
-  color: rgba(0, 0, 0, 0.5);
+  opacity: 0.6;
 
   &:hover {
-    color: rgba(0, 0, 0, 0.6);
+    opacity: 1;
   }
 `;
+
+const Links = styled.div`
+  margin-top: 20px;
+`;
+
+const IconLink = styled.a`
+  margin: 0 10px;
+  color: #000;
+  opacity: 0.5;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const Icon = ({ url, icon }) => {
+  return (
+    <IconLink href={url} target="_blank">
+      {icon}
+    </IconLink>
+  );
+};
+
+const sites = [
+  {
+    url: 'https://github.com/glenpadua',
+    icon: <SiGithub />,
+  },
+  {
+    url: 'https://www.linkedin.com/in/glen-padua/',
+    icon: <SiLinkedin />,
+  },
+  {
+    url: 'https://twitter.com/glenp01',
+    icon: <SiTwitter />,
+  },
+  {
+    url: 'https://www.instagram.com/glen.padua/',
+    icon: <SiInstagram />,
+  },
+];
 
 const Footer = () => {
   return (
     <Wrapper>
-      <Logo src="/assets/logo.png" />
-      <div>
-        Built with{' '}
-        <StyledLink href="https://nextjs.org/" target="_blank">
-          NextJS
-        </StyledLink>
-        {'  '}
-        <span role="img" aria-label="heart on fire">
-          ❤️‍🔥
-        </span>{' '}
-        <StyledLink href="https://prismic.io/" target="_blank">
-          Prismic
-        </StyledLink>
-      </div>
-      <div></div>
+      <Text>
+        <Logo src="/assets/logo.png" />
+        <div>
+          Built with{' '}
+          <StyledLink
+            href="https://nextjs.org/"
+            target="_blank"
+            style={{ color: '#0077fe' }}
+          >
+            NextJS
+          </StyledLink>
+          <span> & </span>
+          <StyledLink
+            href="https://prismic.io/"
+            target="_blank"
+            style={{ color: '#5163ba' }}
+          >
+            Prismic
+          </StyledLink>
+          {'  '}
+          <span role="img" aria-label="heart on fire">
+            ❤️‍🔥
+          </span>{' '}
+        </div>
+      </Text>
+      <Links>
+        {sites.map(site => (
+          <Icon icon={site.icon} url={site.url} key={site.url} />
+        ))}
+      </Links>
     </Wrapper>
   );
 };
